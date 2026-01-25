9aa1c7feb4288462ba5de1180a1de0613a52380b
zhou <zhouzhou.zhang.24@ucl.ac.uk>
2026-01-25 23:04:13 +0000

    Add more questions.

---
+++ src/two_pointers/container_with_most_water.cc
@@ -0,0 +1,25 @@
+#include "deps.h"
+
+class Solution
+{
+  public:
+    int maxArea(vector<int> &heights)
+    {
+        int left = 0, right = heights.size() - 1;
+        int maxWater = 0;
+
+        while (left < right)
+        {
+            int h = min(heights[left], heights[right]);
+            int width = right - left;
+            maxWater = max(maxWater, h * width);
+
+            if (heights[left] < heights[right])
+                ++left;
+            else
+                --right;
+        }
+
+        return maxWater;
+    }
+};
